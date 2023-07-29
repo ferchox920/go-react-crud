65b077dddf23be990522f606ad5e6b53cf611ab7
ferchox920 <fernandoramones92@gmail.com>
2023-07-29 03:37:13 -0300

    fix id

--- models/user-model.go
+++ models/user-model.go
@@ -1,5 +1,8 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-	Name string `json:"name"`
+	Id   primitive.ObjectID `json:"_id" bson:"_id"`
+	Name string             `json:"name"`
 }
